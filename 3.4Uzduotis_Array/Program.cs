fc7896bf25ee1ffd487edfee4c7a9362f827742b
Laura <Laura@DESKTOP-BESSB6E>
2023-09-19 21:33:22 +0300

    Add project files.

---
+++ 3.4Uzduotis_Array/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _3._4Uzduotis_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = { 1, 2, 3 };
+
+            int[] result = RemoveElement(numbers);
+
+            for (int k = 0; k < result.Length; k++)
+            {
+                Console.WriteLine(result[k]);
+            }
+        }
+
+        public static int[] RemoveElement(int[] numbers)
+        {
+            int[] newArray = new int[numbers.Length - 1];
+
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                newArray[i] = numbers[i];
+            }
+            return newArray;
+        }
+    }
+}
